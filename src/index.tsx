1935fe3d3263f7d560dc71696cb9c14ccd45c447
krapli441 <tlfqj1208@gmail.com>
2023-06-06 20:17:16 +0900

    css 선택자에 따라 다른 이름을 적어줘야 한다. div id={Style.root}을 적는 것으로 오류 해결

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 // ? 리액트 라이브러리
 import React from "react";
-import { createRoot, Root } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 // import { BrowserRouter } from "react-router-dom";
 
 // ? 리액트 컴포넌트
 import App from "./App";
-// import "./components/css/root";
+import "./components/css/style.module.css";
 
 // ? CSS 모듈
 import Style from "./components/css/style.module.css";
@@ -13,7 +13,7 @@ import Style from "./components/css/style.module.css";
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <div className={Style.root}>
+  <div id={Style.root}>
     <App />
   </div>
 );
