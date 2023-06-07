dc0803306ac4d11a6453c02d700c206d1fd336e9
krapli441 <tlfqj1208@gmail.com>
2023-06-07 21:40:10 +0900

    alphavantage의 주식 정보를 json에 저장하려고 시도.

---
+++ src/models/getStockData.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+
+const apiKey: string = "4IED2HP9THEKZPFG";
+
+async function getIntervalInfo(symbol: string) {
+  try {
+    const intervalResponse = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${apiKey}`
+    );
+    const responseData = await intervalResponse.json(); // JSON 데이터로 응답을 분석함
+    const timeSeries = responseData["Time Series (5min)"]; // 5분마다의 주식 정보를 포함하는 객체
+    await fs.writeFile(
+      "./data/AAPL.json",
+      JSON.stringify(responseData),
+      (error) => {
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+    for (const key in timeSeries) {
+      const intervalData = timeSeries[key];
+      console.log(`시간 : ${timeSeries[key]}`);
+      console.log(`시가 : ${intervalData["1. open"]}`);
+      console.log(`시가 : ${intervalData["4. close"]}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+setInterval(() => {
+  getIntervalInfo("AAPL");
+}, 1000); // 요청 한 건당 20초의 대기 시간이 필요한다고 했으니 25초 뒤에 실행하도록 함.
+
+//
---
+++ src/models/virtualStockData.ts
@@ -0,0 +1,41 @@
+// 주식 데이터를 넣어보기?
+
+const southKoreaStock = [
+  {
+    id: 1,
+    stockName: "애플",
+    stockPrice: "177.83 USD",
+    stockChangePercentage: "-1.73%",
+    stockChartGraph: "GRAPH",
+  },
+  {
+    id: 2,
+    stockName: "테슬라",
+    stockPrice: "1277.83 USD",
+    stockChangePercentage: "+4.73%",
+    stockChartGraph: "GRAPH",
+  },
+  {
+    id: 3,
+    stockName: "넷플릭스",
+    stockPrice: "17.83 USD",
+    stockChangePercentage: "-1.73%",
+    stockChartGraph: "GRAPH",
+  },
+  {
+    id: 4,
+    stockName: "삼성전자",
+    stockPrice: "57.23 USD",
+    stockChangePercentage: "-6.13%",
+    stockChartGraph: "GRAPH",
+  },
+  {
+    id: 5,
+    stockName: "메타",
+    stockPrice: "251.93 USD",
+    stockChangePercentage: "-21.73%",
+    stockChartGraph: "GRAPH",
+  },
+];
+
+export { southKoreaStock };
